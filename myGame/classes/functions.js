32cccbd81ef5a36b98c43c760921842f8de752a0
SabCool <sabine.classnitz@tumo.center>
2023-12-08 19:43:56 +0100

    partial sever game

--- myGame/classes/functions.js
+++ myGame/classes/functions.js
@@ -1,7 +1,8 @@
 const findRandomPosFor = (obj, symbol) => {
     let fields = obj.findFields(symbol);
     if(fields.length > 0){
-        return random(fields);
+        let randomIndex = Math.floor(Math.random() * fields.length);
+        return fields[randomIndex];
     }else{
         return undefined;
     }
